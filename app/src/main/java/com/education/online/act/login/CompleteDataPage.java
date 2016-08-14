67f70d062d2b4c1f082f23510f8eb78e617f6d92
gaozhiqun <gaozhiqunzz@163.com>
2016-08-14 14:54:24 +0800

    布局xml文件

--- app/src/main/java/com/education/online/act/login/CompleteDataPage.java
+++ app/src/main/java/com/education/online/act/login/CompleteDataPage.java
@@ -10,6 +10,6 @@ public class CompleteDataPage extends BaseFrameAct {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.completedata_page);
+        setContentView(R.layout.fullfilldata_page);
     }
 }
